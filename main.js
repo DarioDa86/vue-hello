eb1c42fe0ab9b98f752a997550f7d2799895279b
DarioDa86 <dario.dagnelli@gmail.com>
2021-09-16 19:03:28 +0200

    immagine inserita

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const app = new Vue(
         el: "#root",
         data: {
             titolo: "Ecco il mio primo titolo con VueJs",
+            imgUrl: 'https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg'
         }
     }
 );
